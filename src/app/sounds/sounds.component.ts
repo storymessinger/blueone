989a0d39e1be65e2019e7221d3803d47c7e92a95
박준동 <jundong@bagjundongs-MacBook-Pro.local>
2018-04-19 16:12:19 +0900

    fix

--- src/app/sounds/sounds.component.ts
+++ src/app/sounds/sounds.component.ts
@@ -18,17 +18,17 @@ export class SoundsComponent implements OnInit {
       speed: 300,
       infinite: true,
       autoplaySpeed: 3000,
-      arrows: true,
       slidesToShow: 1,
       slidesToScroll: 2,
       fade: true,
       centerMode: true,
       centerPadding: '0px',
-      touchMove: true,
       swipeToSlide: true,
-      prevArrow: '<button style="border:none; outline:none; padding:0; display:block; transform:unset; -webkit-transform:unset; position: fixed; height: 220px;width: 150px;top: 0px;left: 0;background: transparent;color: transparent;z-index: 1000;" type="button" class="">Previous</button>',
-      nextArrow: '<button style="border:none; outline:none; padding:0; display:block; transform:unset; -webkit-transform:unset; position: fixed; height: 220px;width: 150px;top: 0px;right: 0;background: transparent;color: transparent;z-index: 1000;" type="button" class="">Next</button>',
       appendDots: $('.dots')
+      // touchMove: true,
+      // arrows: true,
+      // prevArrow: '<button style="border:none; outline:none; padding:0; display:block; transform:unset; -webkit-transform:unset; position: fixed; height: 220px;width: 150px;top: 0px;left: 0;background: transparent;color: transparent;z-index: 1000;" type="button" class="">Previous</button>',
+      // nextArrow: '<button style="border:none; outline:none; padding:0; display:block; transform:unset; -webkit-transform:unset; position: fixed; height: 220px;width: 150px;top: 0px;right: 0;background: transparent;color: transparent;z-index: 1000;" type="button" class="">Next</button>',
     
     })
 
